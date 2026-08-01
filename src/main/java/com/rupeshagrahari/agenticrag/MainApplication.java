fc6863011e5a9c05ca33b67d3950a4e79e95dd7b
Rupesh Agrahari <rupesha157@gmail.com>
2026-07-31 17:18:46 -0700

    renamed main application

--- src/main/java/com/rupeshagrahari/agenticrag/MainApplication.java
+++ src/main/java/com/rupeshagrahari/agenticrag/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgenticRagMcpAssistantApplication {
+public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AgenticRagMcpAssistantApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 
 }
